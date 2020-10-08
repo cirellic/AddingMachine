e08d5102f0f392427794a0d59dac5ccbb34c86c0
clairecirelli <clairecirelli@clairecirelli-XPS-13-9370>
2020-10-07 23:05:03 -0700

    Change number 1 to this super cool project!

--- 360_hw1/src/cse360assignment02/AddingMachine.java
+++ 360_hw1/src/cse360assignment02/AddingMachine.java
@@ -1,26 +1,38 @@
+// Assignment 2: CSE 360
+// Claire Cirelli
+// 1214574634 | cmcirell
+
 package cse360assignment02;
 
 public class AddingMachine {
   private int total;
+  private String show_calc;
   
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
+    show_calc = "0";
   }
   
   public int getTotal () {
-    return 0;
+    return total;
   }
   
   public void add (int value) {
+	  total += value;
+	  show_calc += " + " + value;
   }
 
   public void subtract (int value) {
+	  total += value;
+	  show_calc += " - " + value;
   }
 
   public String toString () {
-    return "";
+	  return show_calc;
   }
 
   public void clear() {
+	  total = 0;
+	  show_calc = "0";
   }
 }
